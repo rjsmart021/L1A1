4d84e94dba0fb3a91e32762874a40de7e285d249
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-08-22 19:47:07 -0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,61 @@
+Module 2 Lesson 1: Assignment | Python Syntax
+Showcase Your Dance Moves!
+ 
+[ ]
+ #Task 1: Code Correction Below is a piece of Python code with incorrect indentation. Your task is to correct it
+ 
+[ ]
+ weather = "sunny"
+
+if weather == "sunny":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+
+Wear sunglasses!
+ 
+[ ]
+ #Task 2: Your Mood Today Ask the user how they feel today.
+ 
+[8]
+0s
+input("How do you feel today")
+  if "Happy":
+    print("That's great to hear!")
+  elif "sad":
+    print("I hope your day gets better!")
+  else: 
+   print("TRY ONE MORE TIME")
+
+Next steps:
+
+Explain error
+2.Python Naming Convention Practice
+ #Task 1: Code Correction
+
+ pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAXLIMIT = 1000
+ 
+ #3. Arithmetic Operations in Daily
+ 
+ #Task 1: Grocery Store Math
+
+ bread = 3
+Pickle = 2.50
+Jelly = 2
+print("Your total will be")
+my_sum = bread + Pickle + Jelly
+print(my_sum)
+ 
+ #Task 2: Bank Interest
+ 
+
+Principle= 10,000
+Interest= 8
+Time= 1
+Simple = (PrincipleInterestTime)/100
+print(simple)
+
+#5.Exploring Logial Operations and Precedence#Task 1:Validating Calculationsa=5b=6c=12my_t = a+b+cprint(my_t)print(my_t/2)
