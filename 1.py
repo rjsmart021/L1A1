cc1e30a13289ef80a35815038cc7c516fef5cd8d
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-08-25 23:48:14 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,48 +1,41 @@
-Module 2 Lesson 1: Assignment | Python Syntax
-Showcase Your Dance Moves!
+##Showcase Your Dance Moves!
  
-[ ]
+
  #Task 1: Code Correction Below is a piece of Python code with incorrect indentation. Your task is to correct it
  
-[ ]
- weather = "sunny"
+
+weather = "sunny"
 
 if weather == "sunny":
-  print("Wear sunglasses!")
+    print("Wear sunglasses!")
 else:
-  print("Take an umbrella!")
+    print("Take an umbrella!")
 
-Wear sunglasses!
+#Wear sunglasses!
+#Task 2: Your Mood Today Ask the user how they feel today.
  
-[ ]
- #Task 2: Your Mood Today Ask the user how they feel today.
- 
-[8]
-0s
 input("How do you feel today")
-  if "Happy":
+if "Happy":
     print("That's great to hear!")
-  elif "sad":
+elif "sad":
     print("I hope your day gets better!")
-  else: 
+else: 
    print("TRY ONE MORE TIME")
 
-Next steps:
 
-Explain error
-2.Python Naming Convention Practice
+
+#2.Python Naming Convention Practice
  #Task 1: Code Correction
 
- pi_value = 3.14
+pi_value = 3.14
 user_age = 25
 user_location = "New York"
 MAXLIMIT = 1000
  
  #3. Arithmetic Operations in Daily
- 
  #Task 1: Grocery Store Math
 
- bread = 3
+bread = 3
 Pickle = 2.50
 Jelly = 2
 print("Your total will be")
@@ -51,11 +44,11 @@ print(my_sum)
  
  #Task 2: Bank Interest
  
-
 Principle= 10,000
-Interest= 8
+Interest= 0.07
 Time= 1
-Simple = (PrincipleInterestTime)/100
-print(simple)
+Simple = (Principle*Interest*Time)/100
+print(Simple)
+Total = Principle + Simple
+print(Total)
 
-#5.Exploring Logial Operations and Precedence#Task 1:Validating Calculationsa=5b=6c=12my_t = a+b+cprint(my_t)print(my_t/2)
